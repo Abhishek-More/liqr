11b737966b6de9f4de0606e14f2c2211fdf88798
Abhishek <abhimore74@gmail.com>
2022-12-26 19:01:59 +0800

    metadata

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use local_ip_address::local_ip;
 use qr2term::print_qr;
 
 #[derive(Parser)]
-#[clap(author="Abhishek More", version, about="Generates a QR code for [local_ip]:[port]")]
 struct Cli {
     #[clap(default_value_t=5500, value_parser=clap::value_parser!(i32).range(0...65535))]
     port: i32,
 }
 
 fn main() {
-    let args: Cli = Cli::parse();
+    let args: Cli = Cli::parse(); 
 
     let ip = local_ip().unwrap();
     let url = format!("https://{}:{}", ip, args.port);
