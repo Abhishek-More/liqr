df5ba2b4f4d7fe5ee8957731e7e2a1ce8f88537c
Abhishek <abhimore74@gmail.com>
2022-12-26 18:45:12 +0800

    added qr and validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+use local_ip_address::local_ip;
+use qr2term::print_qr;
+
+#[derive(Parser)]
+#[clap(author="Abhishek More", version, about="Generates a QR code for [local_ip]:[port]")]
+struct Cli {
+    #[clap(default_value_t=5500, value_parser=clap::value_parser!(i32).range(0...65535))]
+    port: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Cli = Cli::parse();
+
+    let ip = local_ip().unwrap();
+    let url = format!("https://{}:{}", ip, args.port);
+    println!("QR Code for {}", url);
+
+    print_qr(url).unwrap();
 }
