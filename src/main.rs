ba1e8652f8df3d382151fe354895282bc7033c2e
Abhishek <abhimore74@gmail.com>
2023-01-14 20:08:44 +0800

    changed to http

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ fn main() {
     let args: Cli = Cli::parse(); 
 
     let ip = local_ip().unwrap();
-    let url = format!("https://{}:{}", ip, args.port);
+    let url = format!("http://{}:{}", ip, args.port);
+    print! ("\x1B[2J\x1B[1;1H");
     println!("QR Code for {}", url);
 
     print_qr(url).unwrap();
