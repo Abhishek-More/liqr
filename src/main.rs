6e649b77f4a01d4216d1e6f9b2bdfa06b78cf749
Abhishek More <abhimore74@gmail.com>
2023-04-06 20:07:39 -0500

    added general url input

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,27 @@ use qr2term::print_qr;
 struct Cli {
     #[clap(default_value_t=5500, value_parser=clap::value_parser!(i32).range(0...65535))]
     port: i32,
+
+    #[clap(short, long)]
+    url: Option<String>,
 }
 
 fn main() {
     let args: Cli = Cli::parse(); 
+    let url;
+    let arg_url = args.url.unwrap_or("".to_string());
 
-    let ip = local_ip().unwrap();
-    let url = format!("http://{}:{}", ip, args.port);
+
+    if arg_url == "" {
+        let ip = local_ip().unwrap();
+        url = format!("http://{}:{}", ip, args.port);
+    } else {
+        url = arg_url;
+    }
+
+    //Clear the terminal and display the QR code
     print! ("\x1B[2J\x1B[1;1H");
-    println!("QR Code for {}", url);
+    println!("QR Code for {}", url); 
 
     print_qr(url).unwrap();
 }
